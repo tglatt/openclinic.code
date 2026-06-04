517aeb03cb5d8674d62f4dda4212589b0f8b4e17
Thomas Glatt <tglatt@gmail.com>
2026-06-04 12:27:38 +0200

    chore(build): add missing class

---
+++ src/be/openclinic/finance/Sum.java
@@ -0,0 +1,24 @@
+package be.openclinic.finance;
+
+public class Sum {
+    private double sum;
+    private String fname;
+    private String lName;
+    private String personid;
+    private String paInv;
+
+    public double getSum() { return sum; }
+    public void setSum(double sum) { this.sum = sum; }
+
+    public String getFname() { return fname; }
+    public void setFname(String fname) { this.fname = fname; }
+
+    public String getlName() { return lName; }
+    public void setlName(String lName) { this.lName = lName; }
+
+    public String getPersonid() { return personid; }
+    public void setPersonid(String personid) { this.personid = personid; }
+
+    public String getPaInv() { return paInv; }
+    public void setPaInv(String paInv) { this.paInv = paInv; }
+}
